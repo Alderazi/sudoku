3e65cc64d9a8f2d55e1a4ac9121a0a0465ad835f
Alderazi <qm8222606@gmail.com>
2025-07-02 19:13:05 +0300

    adding generated number for sudoku

--- play.js
+++ play.js
@@ -1,14 +1,62 @@
 // variables
 let mistakes=0
-const button=document.querySelectorAll('button') 
+const cell=document.querySelectorAll('.smallBox') 
+const biggerBoxDivs=document.querySelectorAll('.bigBox')
+const soulution=[
+     [5,3,6,1,7,2,8,9,4],
+     [8,2,7,9,6,4,1,5,3],
+     [9,4,1,3,5,8,2,6,7],
+     [7,1,5,6,4,3,9,2,8],
+     [3,4,9,7,8,2,5,1,6],
+     [8,2,6,1,9,5,7,3,4],
+     [4,8,1,3,6,9,2,5,7],
+     [2,9,5,4,7,1,6,3,8],
+     [6,7,3,5,8,2,4,1,9]
+
+]
+const generated=   
+    [['1','',6,'',7,'',8,'',''],
+     ['','','','',6,'',1,'',3],
+     ['','',1,'',5,'',2,'',''],
+     ['','',5,'',4,'','','',8],
+     ['',4,'',7,'',2,'',1,''],
+     [8,'','','',9,'',7,'',''],
+     ['','',1,'',6,'',2,'',''],
+     [2,'',5,'',7,'','','',''],
+     ['','',3,'',8,'',4,'','2']]
 
 
 // functions
+
+const generate=()=>{
+for (let i = 0; i < generated.length; i++) {
+    for (let j = 0; j < generated[i].length; j++) {
+        let index = i * 9 + j;  
+        cell[index].innerText = generated[i][j]; 
+    }
+}
+
+}
 const mistakeHappend=()=>{
     mistakes++;
     document.querySelector("h3").innerText=(`mistakes:${mistakes} /3`)
+    
+    
+    isLose()
 
 }
-   for(let i=0;i<button.length;i++){
-    button[i].addEventListener('click',mistakeHappend)
+const theIsNumber=()=>{}
+const isLose=()=>{
+    if (mistakes===3){
+
+setTimeout(function() {
+  alert("game over you lost");
+}, 10);   
+ }
+
+}
+   for(let i=0;i<cell.length;i++){
+    
+    cell[i].addEventListener('click',mistakeHappend)
    }
+generate()
