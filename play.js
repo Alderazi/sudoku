ff4a18e4bd9ab72fa6cd9355caf43561749f6d52
Alderazi <qm8222606@gmail.com>
2025-07-08 17:18:23 +0300

    I have make the code more clearer and look pretty using prettier

--- play.js
+++ play.js
@@ -1,213 +1,181 @@
 // variables
-let mistakes=0
-const cell=document.querySelectorAll('.smallBox') 
-const boxNumber=document.querySelectorAll('.Number')
-const Reset=document.querySelector('.reset')
-const musicButton=document.querySelector(".musicButton")
-let selctedID=null
-let selectedCell = null
-let isItemSeected =false;
-let emptyString=0
-let filledCorrect=0
-let music=false
-const bgMusic = document.getElementById('bgMusic');
-const soulution=[
-     [5,3,6,1,7,2,8,9,4],
-     [8,2,7,9,6,4,1,5,3],
-     [9,4,1,3,5,8,2,6,7],
-     [7,1,5,6,4,3,9,2,8],
-     [3,4,9,7,8,2,5,1,6],
-     [8,2,6,1,9,5,7,3,4],
-     [4,8,1,3,6,9,2,5,7],
-     [2,9,5,4,7,1,6,3,8],
-     [6,7,3,5,8,2,4,1,9]
-
-]
-const generated=
-    //  [[5,3,6,1,7,2,8,9,4],
-    //  [8,2,7,9,6,4,1,5,3],
-    //  [9,4,1,3,5,8,2,6,7],
-    //  [7,1,5,6,4,3,9,2,8],
-    //  [3,4,9,7,8,2,5,1,6],
-    //  [8,2,6,1,9,5,7,3,4],
-    //  [4,8,1,3,6,9,2,5,7],
-    //  [2,9,5,4,7,1,6,3,8],
-    //  [6,7,3,5,8,2,4,'','']]
-
-
-    [['','',6,'',7,'',8,'',''],
-     ['','','','',6,'',1,'',3],
-     ['','',1,'',5,'',2,'',''],
-     ['','',5,'',4,'','','',8],
-     ['',4,'',7,'',2,'',1,''],
-     [8,'','','',9,'',7,'',''],
-     ['','',1,'',6,'',2,'',''],
-     [2,'',5,'',7,'','','',''],
-     ['','',3,'',8,'',4,'','']]
-
+let mistakes = 0;
+const cell = document.querySelectorAll(".smallBox");
+const boxNumber = document.querySelectorAll(".Number");
+const reset = document.querySelector(".reset");
+const musicButton = document.querySelector(".musicButton");
+let selctedID = null;
+let selectedCell = null;
+let isItemSeected = false;
+let emptyString = 0;
+let filledCorrect = 0;
+let music = false;
+const bgMusic = document.getElementById("bgMusic");
+const soulution = [
+  [5, 3, 6, 1, 7, 2, 8, 9, 4],
+  [8, 2, 7, 9, 6, 4, 1, 5, 3],
+  [9, 4, 1, 3, 5, 8, 2, 6, 7],
+  [7, 1, 5, 6, 4, 3, 9, 2, 8],
+  [3, 4, 9, 7, 8, 2, 5, 1, 6],
+  [8, 2, 6, 1, 9, 5, 7, 3, 4],
+  [4, 8, 1, 3, 6, 9, 2, 5, 7],
+  [2, 9, 5, 4, 7, 1, 6, 3, 8],
+  [6, 7, 3, 5, 8, 2, 4, 1, 9],
+];
+const generated = [
+  ["", "", 6, "", 7, "", 8, "", ""],
+  ["", "", "", "", 6, "", 1, "", 3],
+  ["", "", 1, "", 5, "", 2, "", ""],
+  ["", "", 5, "", 4, "", "", "", 8],
+  ["", 4, "", 7, "", 2, "", 1, ""],
+  [8, "", "", "", 9, "", 7, "", ""],
+  ["", "", 1, "", 6, "", 2, "", ""],
+  [2, "", 5, "", 7, "", "", "", ""],
+  ["", "", 3, "", 8, "", 4, "", ""],
+];
 
 // functions
 
 //check weather there is Number or not
-const thereIsNumber=(event)=>{
-    const Num=event.target.innerText
-    selctedID=Num
-selecting(event)
-}
+const thereIsNumber = (event) => {
+  const num = event.target.innerText;
+  selctedID = num;
+  selecting(event);
+};
 
 // make sure that when hover over number it will not display cursor on the cell
-const selecting=(cell)=>{
-    const text =cell.target.innerText
-    if(!(text==='')){
-    cell.target.style.cursor = 'auto';
-    }
-
-}
+const selecting = (cell) => {
+  const text = cell.target.innerText;
+  if (!(text === "")) {
+    cell.target.style.cursor = "auto";
+  }
+};
 // when selecting cell that there is no number on it it will display click effect and background color
-const isSelcted=(event)=>{
-    const cell=event.target
-    if(isItemSeected===true && selectedCell===cell&& cell.innerText===''){
-        unSelectElemnt(cell)
-    }
-    else if(isItemSeected===true && !(selectedCell===cell )&& cell.innerText===''){
-        selectNewElement(selectedCell,cell)
-
-    }else if(isItemSeected===false && cell.innerText=== ''){
-        selctElement(cell)
-    }
-
-}
+const isSelcted = (event) => {
+  const cell = event.target;
+  if (
+    isItemSeected === true &&
+    selectedCell === cell &&
+    cell.innerText === ""
+  ) {
+    unSelectElemnt(cell);
+  } else if (
+    isItemSeected === true &&
+    !(selectedCell === cell) &&
+    cell.innerText === ""
+  ) {
+    selectNewElement(selectedCell, cell);
+  } else if (isItemSeected === false && cell.innerText === "") {
+    selctElement(cell);
+  }
+};
 // unselect element if it is alreay been selected before
-const unSelectElemnt=(event)=>{
-    event.classList.remove("selected")
-    isItemSeected=false
-    selectedCell=null
-}
+const unSelectElemnt = (event) => {
+  event.classList.remove("selected");
+  isItemSeected = false;
+  selectedCell = null;
+};
 // select element if nothing been selected
-const selctElement=(event)=>{
-    event.classList.add("selected")
-    isItemSeected=true
-    selectedCell=event
-}
+const selctElement = (event) => {
+  event.classList.add("selected");
+  isItemSeected = true;
+  selectedCell = event;
+};
 // when element already selected but the user select new one it will switch to the new one
-const selectNewElement=(oldCell,newCell)=>{
-    selectedCell=newCell
-    oldCell.classList.remove("selected")
-    newCell.classList.add("selected")
-    isItemSeected=true
-}
+const selectNewElement = (oldCell, newCell) => {
+  selectedCell = newCell;
+  oldCell.classList.remove("selected");
+  newCell.classList.add("selected");
+  isItemSeected = true;
+};
 
 // generate numbers from generated array to the cells
-const generate=()=>{
-for (let i = 0; i < generated.length; i++) {
+const generate = () => {
+  for (let i = 0; i < generated.length; i++) {
     for (let j = 0; j < generated[i].length; j++) {
-        let index = i * 9 + j;  
-        cell[index].innerText = generated[i][j]; 
-        if(cell[index].innerText===''){
-            emptyString++
-        }
+      let index = i * 9 + j;
+      cell[index].innerText = generated[i][j];
+      if (cell[index].innerText === "") {
+        emptyString++;
+      }
     }
-}
-
-}
+  }
+};
 // when player makes mistake it will increase
-const mistakeHappend=()=>{
-    mistakes++;
-    document.querySelector("h3").innerText=(`mistakes:${mistakes} /3`)
-    
-    
-    isLose()
+const mistakeHappend = () => {
+  mistakes++;
+  document.querySelector("h3").innerText = `mistakes:${mistakes} /3`;
 
-}
+  isLose();
+};
 // when the limit of mistakes have been reached max number
-const isLose=()=>{
-    if (mistakes>=3){
-
-setTimeout(function() {
-    
-
-}, 500);   
+const isLose = () => {
+  if (mistakes >= 100) {
+    setTimeout(function () {}, 500);
     location.reload();
-
- }
-
-}
+  }
+};
 
 //return the number from the box
-const numberSelected =(event)=>{
-    const number=event.target.innerText
-    if(selectedCell!==null){
-        printNumber(number)
-        checkSolution(selectedCell)
-    }
-
-}
-
-
+const numberSelected = (event) => {
+  const number = event.target.innerText;
+  if (selectedCell !== null) {
+    printNumber(number);
+    checkSolution(selectedCell);
+  }
+};
 
 //print the number in the box
-const printNumber=(numNum)=>{    
-        selectedCell.innerText = numNum;
-        
+const printNumber = (selectedNumber) => {
+  selectedCell.innerText = selectedNumber;
+};
+const checkSolution = (event) => {
+  const selectedCellID = event.getAttribute("id");
+  const insideCell = parseInt(event.innerText);
+  const row = Math.floor(selectedCellID / 9);
+  const column = selectedCellID % 9;
+  const rightAnswer = parseInt(soulution[row][column]);
+
+  if (insideCell === rightAnswer) {
+    event.style.color = "green";
+    unSelectElemnt(event);
+    filledCorrect++;
+  } else {
+    event.style.color = "red";
+    mistakeHappend();
+    setTimeout(() => {
+      event.innerText = "";
+    }, 500);
+  }
+  setTimeout(checkVictory, 1);
+};
+const checkVictory = () => {
+  if (filledCorrect === emptyString) {
+    alert("you won");
+  }
+};
+const resetButton = () => {
+  location.reload();
+};
+const playMusic = () => {
+  music = !music;
+  if (music) {
+    bgMusic.play();
+    musicButton.innerText = "music";
+  } else {
+    musicButton.innerText = "no music";
+    bgMusic.pause();
+  }
+};
+
+for (let i = 0; i < cell.length; i++) {
+  cell[i].addEventListener("click", isSelcted);
+  cell[i].addEventListener("mouseover", thereIsNumber);
 }
-const checkSolution=(event)=>{
-    
-
-        const selectedCellID=event.getAttribute("id")
-        const insideCell=parseInt(event.innerText)
-        const row =Math.floor(selectedCellID/9)
-        const column=selectedCellID%9
-        const rightAnswer=parseInt(soulution[row][column])
-
-        if(insideCell===rightAnswer){
-            event.style.color="green"
-            unSelectElemnt(event)
-            filledCorrect++
-        }
-        else{
-            event.style.color="red"
-            mistakeHappend()
-            setTimeout(()=>{event.innerText=''} ,500)
-        }
-        setTimeout(checkVictory,1)
-
-
-
-        
+for (let j = 0; j < boxNumber.length; j++) {
+  boxNumber[j].addEventListener("click", numberSelected);
 }
-const checkVictory=()=>{
-    if(filledCorrect===emptyString){
-        alert("you won")
-    }
- 
-} 
-const resetButton=()=>{
-    location.reload();
-}
-const playMusic=()=>{
+reset.addEventListener("click", resetButton);
+musicButton.addEventListener("click", playMusic);
 
-    music=!music
-    if(music){
-        bgMusic.play()
-musicButton.innerText="music"
-    }
-
-else{
-    musicButton.innerText="no music"
-    bgMusic.pause()
-}
-}
-
-   for(let i=0;i<cell.length;i++){
-    cell[i].addEventListener('click',isSelcted)
-    cell[i].addEventListener('mouseover',thereIsNumber)
-   }
-   for (let j=0;j<boxNumber.length;j++){
-    boxNumber[j].addEventListener('click',numberSelected)
-   }
-   Reset.addEventListener('click',resetButton)
-   musicButton.addEventListener('click',playMusic)
-
-generate()
-
-
+generate();
